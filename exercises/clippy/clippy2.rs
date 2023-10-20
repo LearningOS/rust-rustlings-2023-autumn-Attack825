6f4bc3665a367ca043c859988d1b0b0657f13938
Attack825 <2707138687@qq.com>
2023-10-20 21:38:19 +0800

    add 100 points

--- exercises/clippy/clippy2.rs
+++ exercises/clippy/clippy2.rs
@@ -7,16 +7,8 @@
 fn main() {
     let mut res = 42;
     let option = Some(12);
-    // 这里我们需要使用if let来判断option是否为Some，否则会报错，因为我们不能直接使用Some来进行判断
-    // 或者我们可以使用while let来进行判断
-    // if let Some(x) = option {
-    //     res += x;
-    // }
-    while let Some(x) = option {
+    if let Some(x) = option {
         res += x;
     }
-    // for x in option {
-    //     res += x;
-    // }
     println!("{}", res);
 }
